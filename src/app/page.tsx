2870e0108e0e2878f39532dd2c5b225e68fdfb79
pastushkov <dimaspas62@gmail.com>
2025-01-17 00:34:27 +0200

    update text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,37 @@
 
 import Button from "@/components/Button/Button";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+const ResetButtonStatus = {
+  default: "Reset Progress",
+  done: "Done",
+};
 
 export default function Home() {
   const router = useRouter();
+  const [resetButtonText, setResetButtonText] = useState(
+    ResetButtonStatus.default
+  );
 
   const handleClearProgress = () => {
     localStorage.clear();
+    setResetButtonText(ResetButtonStatus.done);
+    setTimeout(() => {
+      setResetButtonText(ResetButtonStatus.default);
+    }, 2000);
   };
 
   return (
     <main className="flex flex-col justify-center items-center min-h-screen gap-5">
       <Button onClick={() => router.push("/blur")}>Blur</Button>
       <Button onClick={() => router.push("/fragment")}>Fragmet</Button>
-      <Button onClick={handleClearProgress}>Clear progress</Button>
+      <Button
+        disabled={resetButtonText === ResetButtonStatus.done}
+        onClick={handleClearProgress}
+      >
+        {resetButtonText}
+      </Button>
     </main>
   );
 }
